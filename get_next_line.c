7c5ff6fe8226865a56d41e452e793a71171e5d20
sqrhead <fshelna98@gmail.com>
2025-12-02 16:18:25 +0100

    gnl

---
+++ get_next_line.c
@@ -0,0 +1,13 @@
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+}
+
+int main()
+{
+	printf("GNL\n");
+	char buffer[100] = "ornitorinco"; 
+	size_t nbytes = read(1,buffer,12);
+	printf("bytes : %lu\n",nbytes);
+}
